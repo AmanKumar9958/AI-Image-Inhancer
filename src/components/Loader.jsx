52a385cd44440ce76fb7e8520182037aa32e7227
Aman Kumar <amancollege04@gmail.com>
2025-04-12 14:45:13 +0530

    Added loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Loader = () => {
-    return (
-        <div>Loading</div>
-    )
-}
+    const loading = true;
+    const color = "#ffffff";
 
-export default Loader
+    return (
+        <div className="sweet-loading flex flex-col justify-center items-center mt-10">
+            <ClipLoader
+                color={color}
+                loading={loading}
+                size={50}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    );
+};
+
+export default Loader;
